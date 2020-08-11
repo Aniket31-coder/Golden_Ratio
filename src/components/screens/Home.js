e6395167a0560378c48d0e0ca32f037b93e39c3d
Aniket Dewnani <aniketdewnani31@gmail.com>
2020-08-11 18:06:49 +0530

    Added Carousel

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import flower from '../images/flower.jpg'
-import Sunflower from '../images/Sunflower.jpg'
-import shell from '../images/shell.jpg'
-import Carousel from 'react-bootstrap/Carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React from "react";
+import flower from "../images/flower.jpg";
+import Sunflower from "../images/Sunflower.jpg";
+import shell from "../images/shell.jpg";
+var Carousel = require("react-responsive-carousel").Carousel;
 
-
-
-const Home = ()=>{
-    return (
-    <div>
+const Home = () => {
+  return (
+    <Carousel showArrows onChange onClickItem onClickThumb showThumbs={false}>
+      <div>
         <img className="image" src={shell} alt="shellimg"></img>
-    </div>
-    );
-}
+      </div>
 
+      <div>
+        <img className="image" src={flower} alt="flowerimg"></img>
+      </div>
+
+      <div>
+        <img className="image" src={Sunflower} alt="Sunflowerimg"></img>
+      </div>
+    </Carousel>
+  );
+};
 
 export default Home;
